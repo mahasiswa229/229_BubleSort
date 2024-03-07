287304effd09e403d9b939e9bf8aac806e881c5d
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-03-07 18:19:00 +0800

    membuat bubblesort

--- Buble Sort 2.0/Buble Sort 2.0.cpp
+++ Buble Sort 2.0/Buble Sort 2.0.cpp
@@ -26,3 +26,16 @@ void input() {
         cout << "data ke-" << (i + 1) << ": ";
         cin >> arr[i];
     }
+}void bubbleSortArray() { //procedur untuk mengurutkan array
+    int pass = 1; //step 1
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) { //step 2
+            if (arr[j] > arr[j + 1]) { //step 3
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass = pass + 1; //step 4 
+    } while (pass < n - 1); //step 5
+
